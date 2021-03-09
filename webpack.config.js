44fb1ac5753da8c7e28c154d72b922fd4d821145
Ahmed khaled <ak812554@gmail.com>
2021-03-09 22:32:03 +0200

    modifications are complete

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
       entry: './src/index.js',
       output: {
         path: path.join(__dirname, 'dist'),
-        publicPath: ' ',
+        publicPath: '/',
         filename: 'main.js'
       },
       mode: 'development',
@@ -18,20 +18,16 @@ module.exports = {
             use: [
               {
                 loader: 'html-loader',
-                options: {
-              minimize: true,
-            }
           }           
         ]
       },
       {
-      test : /\.css$/, 
-      use: [
-        {
-          loader: MiniCssExtractPlugin.loader,
-        },
-        "css-loader",
-      ]
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
     },
     {
       test: /\.(png|svg|jpe?g|gif)$/i,
@@ -41,6 +37,26 @@ module.exports = {
         outputPath: 'images',
       },
     },
+    {
+      test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: [
+          {
+            loader: "file-loader", 
+            options: {
+              name: '[name].[ext]',
+              outputPath: "fonts",
+              esModule: false,
+            }
+          }
+        ]
+    },
+    {
+      test: require.resolve("jquery"),
+      loader: "expose-loader",
+      options: {
+        exposes: ["$", "jQuery"],
+      },
+    } 
     ]
   },
   plugins: [
@@ -48,8 +64,28 @@ module.exports = {
       filename: "index.html",
       template:"./src/index.html",
     }),
+    new HtmlWebpackPlugin({
+      filename: "product.html",
+      template:"./src/product.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "checkout.html",
+      template:"./src/checkout.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "payment.html",
+      template:"./src/payment.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "search.html",
+      template:"./src/search.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "contact.html",
+      template:"./src/contact.html",
+    }),
     new MiniCssExtractPlugin({filename: "css/style.css"}),
-    new OptimizeCssAssetsPlugin({})
+    new OptimizeCssAssetsPlugin({}),
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
